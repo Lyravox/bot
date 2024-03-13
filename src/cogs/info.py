e038aa96c7f2ea9d6c5a7bedee52f829e0e46769
Juniper Watson <lyravox1@gmail.com>
2024-03-13 12:48:34 -0500

    Edit info.py

--- src/cogs/info.py
+++ src/cogs/info.py
@@ -15,5 +15,35 @@ class Information(commands.Cog):
         latency = int(self.bot.latency * 1000)
         await interaction.response.send_message(f"Pong! My latency is {latency} ms!")
         
+    @nextcord.slash_command(description="Sends info about the current server")
+    async def serverinfo(self, interaction: Interaction):
+        guild = interaction.guild
+        name = guild.name
+        await interaction.response.send_message(f"Name: {name}")
+        
+    @nextcord.slash_command(description="Sends info about the given user")
+    async def userinfo(self, interaction: Interaction, member: Member = None):
+        if member is None:
+            member = interaction.user
+        name = member.mention
+        await interaction.response.send_message(name)
+
+    @nextcord.slash_command(description="Sends the given users avatar")
+    async def avatar(self, interaction: Interaction, member: Member = None):
+        if member is None:
+            member = interaction.user
+        name = member.name
+        avatar = member.avatar
+        
+        embed = nextcord.Embed(
+            color=0x4584B6,
+            title=f"{name}'s Avatar:"
+        )
+        embed.set_image(url=avatar)
+        
+        await interaction.response.send_message(embed=embed)
+        
+    
+        
 def setup(bot):
     bot.add_cog(Information(bot))
