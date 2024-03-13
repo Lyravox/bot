a3e7c687bc4c9e22108356ebff2499205a6a7a14
Juniper Watson <lyravox1@gmail.com>
2024-03-13 13:40:49 -0500

    Edit utilites.py

--- src/cogs/utilites.py
+++ src/cogs/utilites.py
@@ -5,6 +5,6 @@ from nextcord import Interaction, Member, SlashOption
 class Utilities(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        
+
 def setup(bot):
     bot.add_cog(Utilities(bot))
